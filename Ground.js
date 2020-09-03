77db970b9631aef1a10b31d837cd336a38504db2
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2020-09-03 22:13:38 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -3,7 +3,7 @@ class Ground {
       var options = {
           isStatic: true
       }
-      this.body = bodies.rectangle(x,y,width,height,options);
+      this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
       World.add(world, this.body);
